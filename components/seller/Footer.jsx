80706740ab272b6fd1fe4b8484d72e220ba6f9c4
HarshithaGnanavel <harshithag.281003@gmail.com>
2025-07-05 09:26:45 +0530

    Final commit modified

--- components/seller/Footer.jsx
+++ components/seller/Footer.jsx
@@ -4,27 +4,34 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-10">
+    <div className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-10 py-6 bg-white">
+      {/* Left Side - Logo and Copyright */}
       <div className="flex items-center gap-4">
-        <Image className="hidden md:block" src={assets.logo} alt="logo" />
-        <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
-        <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-          Copyright 2025 © greatstack.dev All Right Reserved.
+        <Image
+          className="w-24 h-30 md:block"
+          src={assets.logo}
+          alt="logo"
+        />
+        <p className="text-s md:text-sm text-center md:text-left text-gray-500">
+          Copyright 2025 © urbancart.com All Rights Reserved.
         </p>
       </div>
-      <div className="flex items-center gap-3">
+
+      {/* Right Side - Social Icons */}
+      <div className="flex items-center gap-4 mt-4 md:mt-0">
         <a href="#">
-          <Image src={assets.facebook_icon} alt="facebook_icon" />
+          <Image src={assets.facebook_icon} alt="facebook_icon" className="w-5 h-5" />
         </a>
         <a href="#">
-          <Image src={assets.twitter_icon} alt="twitter_icon" />
+          <Image src={assets.twitter_icon} alt="twitter_icon" className="w-5 h-5" />
         </a>
         <a href="#">
-          <Image src={assets.instagram_icon} alt="instagram_icon" />
+          <Image src={assets.instagram_icon} alt="instagram_icon" className="w-5 h-5" />
         </a>
       </div>
     </div>
   );
 };
+
 
 export default Footer;
